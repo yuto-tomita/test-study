2e082b2881de551ef0b354f9473f9534dc904b1f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-02-01 15:43:01 +0900

    lint周り修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,27 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true
+    browser: true,
   },
   extends: [
-    'plugin:tailwindcss/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
-    // "eslint:recommended",
-    'prettier'
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {
+      jsx: false,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   rules: {
     /** eslint options */
     semi: ['error', 'never'],
@@ -28,83 +30,159 @@ module.exports = {
     'space-in-parens': ['error', 'never'],
     'object-curly-spacing': ['error', 'always'],
     'no-self-compare': 'error',
-    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'comma-spacing': [
+      'error',
+      {
+        before: false,
+        after: true,
+      },
+    ],
     'computed-property-spacing': ['error', 'never'],
     'func-call-spacing': ['error', 'never'],
-    'indent': ['error', 2],
-    'key-spacing': ['error', { 'beforeColon': false }],
-    'max-len': ['error', { 'code': 70 }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'key-spacing': [
+      'error',
+      {
+        beforeColon: false,
+      },
+    ],
     'no-multi-spaces': 'error',
     'no-multiple-empty-lines': 'error',
     'no-whitespace-before-property': 'error',
-    'object-curly-spacing': ['error', 'always'],
     quotes: ['error', 'single'],
     'rest-spread-spacing': ['error', 'never'],
+
     /** eslint-plugin-typescript options */
 
     /** vue3-eslint options */
-    'vue/block-lang': ['error', {
-      'script': { 'lang': 'ts' }
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'always'
-    }],
-    'vue/html-closing-bracket-spacing': ['error', {
-      'startTag': 'never',
-      'endTag': 'never',
-      'selfClosingTag': 'always'
-    }],
-    'vue/html-indent': ['error', 2, {
-      'attribute': 1,
-      'baseIndent': 1,
-      'closeBracket': 0,
-      'alignAttributesVertically': true,
-      'ignores': []
-    }],
-    'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'never',
-        'normal': 'always',
-        'component': 'always'
+    'vue/block-lang': [
+      'error',
+      {
+        script: {
+          lang: 'ts',
+        },
       },
-      'svg': 'always',
-      'math': 'always'
-    }],
-    'vue/first-attribute-linebreak': ['error', {
-      'singleline': 'ignore',
-      'multiline': 'below'
-    }],
-    'vue/max-attributes-per-line': ['error', {
-      'singleline': {
-        'max': 1
-      },      
-      'multiline': {
-        'max': 1
-      }
-    }],
-    'vue/multiline-html-element-content-newline': ['error', {
-      'ignoreWhenEmpty': true,
-      'ignores': ['pre', 'textarea'],
-      'allowEmptyLines': true
-    }],
-    'vue/mustache-interpolation-spacing': ['error', 'always'],
-    'vue/no-spaces-around-equal-signs-in-attribute': ['error'],
+    ],
+    // 'vue/html-closing-bracket-newline': ['error', {
+    //   'singleline': 'never',
+    //   'multiline': 'always'
+    // }],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always',
+      },
+    ],
+    // 'vue/html-indent': ['error', 2, {
+    //   'attribute': 1,
+    //   'baseIndent': 1,
+    //   'closeBracket': 0,
+    //   'alignAttributesVertically': true,
+    //   'ignores': []
+    // }],
+    // 'vue/html-self-closing': ['error', {
+    //   'html': {
+    //     'void': 'never',
+    //     'normal': 'always',
+    //     'component': 'always'
+    //   },
+    //   'svg': 'always',
+    //   'math': 'always'
+    // }],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'ignore',
+        multiline: 'below',
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
+    ],
+    // 'vue/multiline-html-element-content-newline': ['error', {
+    //   'ignoreWhenEmpty': true,
+    //   'ignores': ['pre', 'textarea'],
+    //   'allowEmptyLines': true
+    // }],
+    'vue/mustache-interpolation-spacing': [
+      'error',
+      'always',
+    ],
+    'vue/no-spaces-around-equal-signs-in-attribute': [
+      'error',
+    ],
     'vue/prop-name-casing': ['error', 'camelCase'],
     'vue/v-bind-style': ['error', 'shorthand'],
     'vue/v-on-style': ['error', 'shorthand'],
-    'vue/component-options-name-casing': ['error', 'PascalCase'],
-    'vue/html-button-has-type': ['error', {
-      'button': true,
-      'submit': true,
-      'reset': true
-    }],
-    'vue/html-comment-content-spacing': ['error',
+    'vue/component-options-name-casing': [
+      'error',
+      'PascalCase',
+    ],
+    'vue/html-button-has-type': [
+      'error',
+      {
+        button: true,
+        submit: true,
+        reset: true,
+      },
+    ],
+    'vue/html-comment-content-spacing': [
+      'error',
       'always',
       {
-        'exceptions': ['*']
-      }
+        exceptions: ['*'],
+      },
     ],
-    'vue/html-comment-indent': ['error', 2]
-  }
+    'vue/html-comment-indent': ['error', 2],
+    'import/no-unresolved': ['off'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern:
+              '{vue,vue-router,pinia,@vuelidate/**,msw,msw/**,jest,@jest/**,@testing-library/**,axios,js-cookie,}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '{**/*.css,**/*.scss}',
+            group: 'type',
+            position: 'after',
+          },
+          {
+            pattern: '@/**',
+            group: 'sibling',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
 }
