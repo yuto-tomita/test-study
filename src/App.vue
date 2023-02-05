58513057d0bb4f0dac1921a8406d98c7acf95aeb
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-02-05 14:23:36 +0900

    linterが効いてるかチェック

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from '@/components/HelloWorld.vue'
-
 </script>
 
 <template>
@@ -10,10 +9,8 @@ import HelloWorld from '@/components/HelloWorld.vue'
     alt="Vue logo"
     src="./assets/logo.png"
     class="m-auto"
-  >
-  <hello-world
-    msg="Hello tommy boiler template"
   />
+  <HelloWorld msg="Hello tommy boiler template" />
 </template>
 
 <style>
