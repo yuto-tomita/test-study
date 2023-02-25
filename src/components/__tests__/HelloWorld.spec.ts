4f4bf4edab012528e09d135f7c611725b4dac60e
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-02-25 14:13:45 +0900

    lint fix

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,12 +1,13 @@
 import { mount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 import { test, expect } from 'vitest'
+
+import HelloWorld from '@/components/HelloWorld.vue'
 
 test('HelloWorld.vue', () => {
   const wrapper = mount(HelloWorld, {
     props: {
-      msg: 'hello'
-    }
+      msg: 'hello',
+    },
   })
 
   expect(wrapper.text()).toContain('hello')
