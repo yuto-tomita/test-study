2e1765271297debb1ec8fa3d22468d691047bd81
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-03-06 08:15:34 +0900

    TanstackQueryのグローバル登録

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
 import App from './App.vue'
 
 import './assets/index.css'
 import { router } from '@/router'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .use(VueQueryPlugin)
+  .mount('#app')
